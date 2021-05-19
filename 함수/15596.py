4f23e7acedcaf930514fe1cf24a72fdfab7eda9a
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-19 21:34:39 +0900

    15596번 : 정수 N개의 합 설명 추가

--- 함수/15596.py
+++ 함수/15596.py
@@ -1,5 +1,5 @@
 # 15596번 : 정수 N개의 합
-def solve(a):
+def solve(n):  # n를 입력받는 함수
     ans = 0
-    ans += sum(a)
-    return ans
+    ans += sum(n)  # sum() 사용해서 합을 구함
+    return ans  # 정수 n개의 합을 구한 것을 반환
