8bf84db574e1995a3148bc9097489ffc54146619
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-19 22:18:23 +0900

    4673번 : 셀프 넘버 설명 추가

--- 함수/4673.py
+++ 함수/4673.py
@@ -4,7 +4,7 @@ numbers_set = set() # set() 함수 -> 집합으로 중복을 없앰
 for num in numbers: # 1부터 10000까지 반복
     for n in str(num): # num을 str()을 이용하여 문자열로 변환 후 반복(75일 경우 7, 5처럼 하나씩 떼어내기 위해)
         num += int(n) # n을 int()을 이용하여 정수로 변환 후 더함(75일 경우 75+7+5의 값을 저장)
-    numbers_set.add(num) # numbers_set에 자리 수를 분리하여 더한값을 add()을 이용하여 저장함
+    numbers_set.add(num) # numbers_set에 자리 수를 분리하여 더한 값을 add()을 이용하여 저장함(중복을 없앴기에 중복된 값은 제외됨)
 
 self_number = numbers - numbers_set # 생성자가 없는 숫자인 셀프넘버를 구하기 위해 1부터 10000까지와 생성자로 생겨난 수를 빼어 구함
 for self_num in sorted(self_number): # self_numbers를 오름차순으로 정렬함
