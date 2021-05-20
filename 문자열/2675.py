86a0c7572241eb9b168b73d2a0c9a61043f2c021
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-20 14:18:37 +0900

    2675번 : 문자열 반복 설명 추가

--- 문자열/2675.py
+++ 문자열/2675.py
@@ -1,8 +1,8 @@
 # 2675번 : 문자열 반복
-t = int(input())
-for i in range(t):
-    num, s = input().split()
-    text = ""
-    for i in s:
-        text += int(num) * i
-    print(text)
+t = int(input()) # 문자열을 몇 개 입력할 것인지 입력
+for i in range(t): # 문자열을 입력한 개수만큼 반복
+    num, s = input().split() # 문자열을 몇 번 반복한 것인지와 문자열 s 입력
+    text = "" # 문자열로 초기화(문자열 반복을 위해)
+    for i in s: # 입력한 문자열만큼 반복
+        text += int(num) * i # 개수 * 문자열
+    print(text) # 문자열 반복한 것 출력
