a916d44ba1c3e39e1b4f85663a40f3fdce8f9989
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-21 10:18:01 +0900

    1152번 : 단어의 개수 설명 추가

--- 문자열/1152.py
+++ 문자열/1152.py
@@ -1,5 +1,5 @@
 # 1152번 : 단어의 개수
-string = input("")
-words = string.split(" ")
-words = [w for w in words if w != ""]
-print(len(words))
+string = input("") # 문자열 입력받기
+words = string.split(" ") # 입력받은 문자열을 공백 제거
+words = [i for i in words if i != ""] # 공백이 아닌 경우에만 words에 저장
+print(len(words)) # 몇 개의 단어가 있는지 알기 위해 길이 출력
