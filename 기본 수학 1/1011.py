c206e383d5bfb4cdfe2e7aa7e5da0951773ea8b4
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-25 08:31:59 +0900

    1011번 : Fly me to the Alpha Centauri

---
+++ 기본 수학 1/1011.py
@@ -0,0 +1,15 @@
+# 1011번 : Fly me to the Alpha Centauri
+t = int(input())
+
+for _ in range(t):
+    x, y = map(int,input().split())
+    distance = y - x
+    count = 0
+    move = 1
+    move_plus = 0
+    while move_plus < distance :
+        count += 1
+        move_plus += move
+        if count % 2 == 0:
+            move += 1
+    print(count)
