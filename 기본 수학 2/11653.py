2f01297c8acb90aaec89357f6a7faba4f2596eae
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-27 08:15:47 +0900

    11653번 : 소인수분해

---
+++ 기본 수학 2/11653.py
@@ -0,0 +1,12 @@
+# 11653번 : 소인수분해
+number = int(input())
+result = []
+while number != 1:
+    for i in range(2, number + 1):
+        if number % i == 0:
+            result.append(i)
+            number = number // i
+            break
+
+for i in result:
+    print(i)
